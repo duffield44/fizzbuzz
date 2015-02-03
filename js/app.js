d70734c319f4ba82cfb9036d3c20ba2658c35404
duffield44 <duffieldbenjamin44@gmail.com>
2015-02-03 09:45:20 +0800

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+ for (var i = 1; i < 101; i++) {
+   if (i % 15 == 0) {
+   	$('.list').append("<p>fizzbuzz</p>");
+   }
+   else if (i % 5 == 0) {
+   	$('.list').append("<p>Buzz</p>");
+   }
+   else if (i % 3 == 0) {
+   	$('.list').append("<p>Fizz</p>");
+   }
+   else {
+   	$('.list').append("<p>" + i + "</p>");
+   };
+};
+
+
