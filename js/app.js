8aef1537284d166ff218eddfe592a30be0f02b82
duffield44 <duffieldbenjamin44@gmail.com>
2015-02-09 12:27:45 +0800

    Refactor fizzbuzz function. Add cprint Printer function.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,44 @@
 $(document).ready(function(){
- for (var i = 1; i < 101; i++) {
-   if (i % 15 == 0) {
-   	$('.list').append("<p>fizzbuzz</p>");
-   }
-   else if (i % 5 == 0) {
-   	$('.list').append("<p>Buzz</p>");
-   }
-   else if (i % 3 == 0) {
-   	$('.list').append("<p>Fizz</p>");
-   }
-   else {
-   	$('.list').append("<p>" + i + "</p>");
-   };
-};
+ 
+
+function Printer(divId) {
+  var lineCount = 1;
+  
+  this.printLine = function() {
+    var m = lineCount + ': ';
+    
+    for (var i = 0; i < arguments.length; i++) {
+      m += arguments[i] + ' ';
+    }
+
+    var n = document.createElement("pre"),
+        t = document.createTextNode(m);
+
+    n.appendChild(t);
+    document.getElementById(divId).appendChild(n);
+    
+    lineCount++;
+  }
+}
+
+var consolePrinter = new Printer('list');
+var cprint = consolePrinter.printLine;
+
+function fizzbuzz(max) {
+  for ( var i = 1; i <= max; i++ ) {
+    if (i % 5 == 0 && i % 3 == 0) {
+      cprint("FizzBuzz");
+    } else if ( i % 3 == 0 ) {
+      cprint("Fizz");
+    } else if (i % 5 == 0) {
+      cprint("Buzz");
+    } else {
+      cprint(i);
+    }
+  }
+}
+
+fizzbuzz(15);
+
 });
 
